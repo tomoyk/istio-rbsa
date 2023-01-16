7eb9cbbdfc721f8107dc2157f7a4e99b736c3e5b
tomoyk <tmyk.kym@gmail.com>
2023-01-16 02:38:03 +0000

    update runnerfile.sh

--- runnerfile.sh
+++ runnerfile.sh
@@ -1,9 +1,23 @@
 task_default() {
-	kubectl delete -f my-envoy-filter.yml
-	kubectl apply -f my-envoy-filter.yml
+	task_deploy
 }
 
 task_log() {
 	pod_name=$(kubectl get pod -n sock-shop | grep front-end | awk '{print $1}')
 	kubectl logs $pod_name -n sock-shop -c istio-proxy | grep lua
+}
+
+task_logs() {
+	task_log
+}
+
+task_deploy() {
+	kubectl delete -f my-envoy-filter.yml
+	kubectl apply -f my-envoy-filter.yml
+	kubectl get pod
+	kubectl get envoyfilter
+}
+
+task_apply() {
+	task_deploy
 }
