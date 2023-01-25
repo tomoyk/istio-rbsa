669f4f45b8bfb602ca2ee4dae76becb85511961e
tomoyk <tmyk.kym@gmail.com>
2023-01-25 23:38:36 +0000

    add doktor config

--- doktor/runnerfile.sh
+++ doktor/runnerfile.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 task_deploy() {
-	kuebctl apply -f complete-doktor-customize2.yml
+	kubectl apply -f complete-doktor-customize2.yml
 	./configmap-deploy.sh
 	./filter-deploy.sh
 }
 
-task_log() {
+task_log-p() {
 	pod=$(kubectl get pod -n paper | grep app | awk '{print $1}')
-	kubectl logs $pod -n paper -c istio-proxy 
+	echo $pod
+	kubectl logs $pod -n paper -c istio-proxy | tail
 }
 
+task_log-f() {
+	pod=$(kubectl get pod -n front | grep app | awk '{print $1}')
+	echo $pod
+	kubectl logs $pod -n front -c istio-proxy | tail
+}
