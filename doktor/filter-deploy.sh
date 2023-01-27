59b722fe84c29e55a830bbaabdfccf61db57234c
tomoyk <tmyk.kym@gmail.com>
2023-01-27 02:48:26 +0000

    customized filter-deploy for mode switching

--- doktor/filter-deploy.sh
+++ doktor/filter-deploy.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+case "$1" in
+    "proposal")
+        filter_file="template-envoy-filter-proposal.yml"
+        ;;
+    "normal")
+        filter_file="template-envoy-filter.yml"
+        ;;
+    *)
+        exit 1
+        ;;
+esac
+
+echo "filter_file=$filter_file"
+
 list="front paper author fulltext thumbnail stats"
 for x in $list
 do
-    sed "s/REPLACE_ME/$x/g" template-envoy-filter.yml | kubectl apply -f -
+    sed "s/REPLACE_ME/$x/g" $filter_file | kubectl apply -f -
 done
