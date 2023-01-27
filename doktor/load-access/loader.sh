65d315a5e76f7e7cc9f1b43344394a08e57966d9
tomoyk <tmyk.kym@gmail.com>
2023-01-27 02:48:57 +0000

    update loader for latency output and logging support

--- doktor/load-access/loader.sh
+++ doktor/load-access/loader.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+cd `dirname $0`
+
+timestamp=$(date "+%Y-%m-%d-%H-%M-%S")
+filename="load-${timestamp}.log"
+touch $filename
+
 counter=0
 cat url-patterns.txt | while read line
 do
@@ -7,7 +13,7 @@ do
 	method=$(echo $line | awk '{print $1}')
 	path=$(echo $line | awk '{print $2}')
 	echo -n -e "${counter}\t${method}\thttp://localhost${path}\t"
-	curl -I -s -X $method "http://localhost${path}" | head -n 1 | cut -d' ' -f2-
+	curl -I -s -w "%{http_code}\t%{time_total}\n" -X $method -o /dev/null "http://localhost${path}" # | head -n 1 | cut -d' ' -f2-
 	# sleep 0.1
 	counter=$((counter + 1))
-done
+done | tee -a $filename
