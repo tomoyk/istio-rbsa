b43392d0652e8796e358fb1749bb5ab4b70ea2e4
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2023-01-31 20:22:37 +0900

    add result

---
+++ doktor/a.py
@@ -0,0 +1,27 @@
+# n1 = "logs-2023-01-27-05-44-46-n/raw-length1"
+e1 = "logs-2023-01-28-03-23-22-e/raw-length1-average"
+p1 = "logs-2023-01-27-05-25-12-p/raw-length1-average"
+
+with open(e1) as fe:
+    e = fe.read().splitlines()
+with open(p1) as fp:
+    p = fp.read().splitlines()
+
+
+diff_e = {}
+diff_p = {}
+for x in e:
+     k = " ".join(x.split("\t")[0:2])
+     v = x.split("\t")[2]
+     diff_e[k] = v
+for x in p:
+     print(x)
+     k = " ".join(x.split("\t")[0:2])
+     v = x.split("\t")[2]
+     diff_p[k] = v
+
+# 804, 71
+# print(len(diff_e), len(diff_p))
+
+for x in diff_p.keys():
+    print(x + "\t" + str(float(diff_p[x]) - float(diff_e[x])))
