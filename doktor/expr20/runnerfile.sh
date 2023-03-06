090567e25ab28a80655aa277089d68e1788eaafe
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2023-03-06 10:21:46 +0900

    add expr20 result_bson

--- doktor/expr20/runnerfile.sh
+++ doktor/expr20/runnerfile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# jsonbinpack
 task_jbp() {
     # Encoding file create
     ./node_modules/.bin/jsonbinpack compile \
@@ -15,5 +16,14 @@ task_jbp() {
             result_jsonbinpack/_encoding.json \
             "response_collects/$fname" \
             > "result_jsonbinpack/bin.$newfname"
+    done
+}
+
+# ubjson
+task_ubj() {
+    for fname in $(ls response_collects)
+    do
+        newfname=$(echo $fname | sed 's/.txt$/.result/g')
+        json2bson "response_collects/$fname" "result_bson/ubj.$newfname"
     done
 }
