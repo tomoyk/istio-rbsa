6adab04c611102d348354b009628c825d63a125f
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2023-03-16 12:25:48 +0900

    fix sort.py

--- doktor/expr20/response_collects/sort.py
+++ doktor/expr20/response_collects/sort.py
@@ -1,12 +1,15 @@
 import json
 
-with open("fulltext-Wi-Fi.txt") as f:
+basename = "fulltext-Wi-Fi.txt"
+newname = basename.replace(".txt", "2.txt")
+
+with open(basename) as f:
     j = json.load(f)
 
 k = j["fulltexts"]
 m = sorted(k, key=lambda x: x['page_number'])
 j["fulltexts"] = m
 
-print(json.dumps(m, indent=4))
-with open("fulltext-Wi-Fi2.txt", mode='w') as f2:
-    json.dump(m, f2)
+print(json.dumps(m, indent=4, ensure_ascii=False))
+with open(newname, mode='w') as f2:
+    json.dump(m, f2, ensure_ascii=False)
